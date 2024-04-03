2045a76dee967210f77d45053a9efc4487b63159
Rashmi Vagha <rvagha@umass.edu>
2024-04-03 01:33:28 -0400

    Leetcode

--- basics/leetcode_merge_sorted_array.py
+++ basics/leetcode_merge_sorted_array.py
@@ -4,13 +4,14 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         j = 0
-        if len(nums2) != 0:
+        if n!= 0:
             for i in range(0, m+n):
-                if nums1[i] > nums2[j]:
-                    nums1.insert(i, nums2[j])
-                    nums1.pop()
-                    j += 1
-                if nums1[i] == 0:
-                    nums1.insert(i, nums2[j])
-                    nums1.pop()
-                    j += 1
+                if j < n: #because nums2 can be empty
+                    if nums1[i] >= nums2[j]: # example 1,3
+                        nums1.insert(i, nums2[j]) 
+                        nums1.pop()
+                        j += 1
+                    elif i >= m+j: # number of valid elements in nums1 plus number of elements added
+                        nums1.insert(i, nums2[j])
+                        nums1.pop()
+                        j += 1
