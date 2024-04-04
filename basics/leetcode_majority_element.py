a07ebda9b879dadb9e9af41cae13c0bbdf7dcae2
Rashmi Vagha <rvagha@umass.edu>
2024-04-04 03:56:59 -0400

    Leetcode

--- basics/leetcode_majority_element.py
+++ basics/leetcode_majority_element.py
@@ -1,5 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        n = len(nums)
-        return nums[n//2]
+        ele = nums[0]
+        cnt = 0
+        for i in range(0, len(nums)):
+            if nums[i] == ele:
+                cnt += 1
+            else:
+                cnt -= 1
+            
+            if cnt == 0:
+                cnt = 1
+                ele = nums[i]
+        return ele
