e06c4846f0f77ff4104f8d5a4665349ff9b35ac1
Rashmi Vagha <rvagha@umass.edu>
2024-04-08 03:45:43 -0400

    Leetcode

--- basics/leetcode_rotate_array.py
+++ basics/leetcode_rotate_array.py
@@ -1,10 +1,23 @@
 class Solution:
+    def reverse_array(self, nums: List[int], start: int, end: int) -> List[int]:
+        while start <= end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
+
+        return nums
+    
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k = k % len(nums)
+        n = len(nums)
+        k %= n
         
-        for i in range(0,k):
-            temp = nums.pop()
-            nums.insert(0, temp)
+        # for i in range(0,k):
+        #     temp = nums.pop()
+        #     nums.insert(0, temp)
+
+        nums = self.reverse_array(nums, 0, n-1)
+        nums = self.reverse_array(nums, 0, k-1)
+        nums = self.reverse_array(nums, k, n-1)
