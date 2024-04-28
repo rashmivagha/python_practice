a79abf4865ca07a75b5a8f4b57b7196cb907e449
Rashmi Vagha <rvagha@umass.edu>
2024-04-28 01:19:36 -0400

    Best time for stocks

--- basics/leetcode_best_time_forstocks_2.py
+++ basics/leetcode_best_time_forstocks_2.py
@@ -1,6 +1,24 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        int lowest
-        int profit
-        int max_profit = 0
-        return max_profit
+        max_profit = 0
+        profit = 0
+        bought = prices[0]
+        total_profit = 0
+        n = len(prices)
+        for i in range(1, n):
+            if prices[i] < bought:
+                bought = prices[i]
+            elif prices[i] > bought:
+                profit = prices[i] - bought
+                if profit > max_profit:
+                    max_profit = profit
+                    if i == n-1 and total_profit == 0:
+                        total_profit = max_profit
+                else:
+                    total_profit += max_profit
+                    print("Max profit ", max_profit)
+                    print("Total profit ", total_profit)
+                    max_profit = 0
+                    bought = prices[i]
+
+        return total_profit
