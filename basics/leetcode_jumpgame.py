b2824e4a9285fbf045f04bd8183a9e9e703050fb
Rashmi Vagha <rvagha@umass.edu>
2024-05-07 00:45:44 -0400

    Leetcode

--- basics/leetcode_jumpgame.py
+++ basics/leetcode_jumpgame.py
@@ -1,3 +1,10 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        
+        final_positions = []
+        for i in range(0,len(nums)):
+            for j in range(1,i):
+                final_positions.append(j+nums[j])
+        if len(nums)-1 in final_positions :
+            return True
+        else:
+            return False
